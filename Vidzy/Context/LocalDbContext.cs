4f13d946b6e3530ff0d6c375e79c3137db0345dd
André Franciscato Paggi <andre.franciscato@hotmail.com>
2016-09-26 18:50:27 -0300

    Exercise Section 4 Completed!

---
+++ Vidzy/Context/LocalDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vidzy.Model;
+
+namespace Vidzy.Context
+{
+    public class LocalDbContext : DbContext
+    {
+        public LocalDbContext() : base("LocalDbContext")
+        {
+            
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Video> Videos { get; set; }
+    }
+}
